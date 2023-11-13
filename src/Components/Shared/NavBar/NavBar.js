636a457d750ccd0e3bbb5c0d44724ca335722e10
rhnchowdhury <rhnchdri@gmail.com>
2023-11-13 16:33:48 +0600

    navside done

--- src/Components/Shared/NavBar/NavBar.js
+++ src/Components/Shared/NavBar/NavBar.js
@@ -8,30 +8,30 @@ import logo from '../../../assets/logos/head-logo.png';
 const NavBar = () => {
     return (
         <div>
-            <div className="navbar bg-[#4268B1]">
+            <div className="navbar bg-[#4268B1] relative">
                 <div className="navbar-start ml-8">
                     <p className='text-white flex'><span className='mt-1 mr-2'><BsFillTelephoneFill></BsFillTelephoneFill></span> +88 01755 744312</p>
                 </div>
                 <div className="navbar-center">
                     <p className='text-white flex'><span className='mt-1 mr-2'><IoMailSharp></IoMailSharp></span> email@gmail.com</p>
                 </div>
-                <div className="navbar-end mr-8">
-                    <button className="flex text-white space-x-2">
-                        <BsInstagram></BsInstagram>
-                        <BsFacebook></BsFacebook>
-                        <FaTwitter></FaTwitter>
-                        <BsPinterest></BsPinterest>
+                <div className="navbar-end mr-8 ">
+                    <button className="flex text-white space-x-5 bg-[#24316B] absolute py-[22px] px-5">
+                        <BsInstagram className='nav-icon'></BsInstagram>
+                        <BsFacebook className='nav-icon'></BsFacebook>
+                        <FaTwitter className='nav-icon'></FaTwitter>
+                        <BsPinterest className='nav-icon'></BsPinterest>
                     </button>
                 </div>
             </div>
             <div className='flex justify-center mt-8'>
                 <div className='relative'>
-                    <img src={logo} alt="" className='' />
+                    <img src={logo} alt="khukumoni" className='w-[500px]' />
                 </div>
                 <div>
-                    <div className='absolute ml-24'>
-                        <button className=" btn-style px-3 py-1">Sign Up</button>
-                        <button className=" btn-style ml-5 px-4 py-1 ">Log In</button>
+                    <div className='absolute ml-44'>
+                        <button className="nav-btn-style px-3 py-1">Sign Up</button>
+                        <button className="nav-btn-style ml-4 px-4 py-1 ">Log In</button>
                     </div>
                 </div>
             </div>
