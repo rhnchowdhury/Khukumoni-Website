1ee1441d70c5a7967e01e0d8e63d997ecbe4fac5
rhnchowdhury <rhnchdri@gmail.com>
2023-11-13 15:42:28 +0600

    logo add

--- src/Components/Shared/NavBar/NavBar.js
+++ src/Components/Shared/NavBar/NavBar.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import { BsFacebook, BsFillTelephoneFill, BsInstagram, BsPinterest } from "react-icons/bs";
+import { IoMailSharp } from "react-icons/io5";
+import { FaTwitter } from "react-icons/fa";
+import './NavBar.css';
+import logo from '../../../assets/logos/head-logo.png';
 
 const NavBar = () => {
     return (
         <div>
             <div className="navbar bg-[#4268B1]">
-                <div className="navbar-start">
-                    <p className='text-white'>+88 01755 744312</p>
+                <div className="navbar-start ml-8">
+                    <p className='text-white flex'><span className='mt-1 mr-2'><BsFillTelephoneFill></BsFillTelephoneFill></span> +88 01755 744312</p>
                 </div>
                 <div className="navbar-center">
-                    <p className='text-white'>email@gmail.com</p>
+                    <p className='text-white flex'><span className='mt-1 mr-2'><IoMailSharp></IoMailSharp></span> email@gmail.com</p>
                 </div>
-                <div className="navbar-end">
-                    <button className="btn btn-square btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                <div className="navbar-end mr-8">
+                    <button className="flex text-white space-x-2">
+                        <BsInstagram></BsInstagram>
+                        <BsFacebook></BsFacebook>
+                        <FaTwitter></FaTwitter>
+                        <BsPinterest></BsPinterest>
                     </button>
+                </div>
+            </div>
+            <div className='flex justify-center mt-8'>
+                <div className='relative'>
+                    <img src={logo} alt="" className='' />
+                </div>
+                <div>
+                    <div className='absolute ml-24'>
+                        <button className=" btn-style px-3 py-1">Sign Up</button>
+                        <button className=" btn-style ml-5 px-4 py-1 ">Log In</button>
+                    </div>
                 </div>
             </div>
         </div>
