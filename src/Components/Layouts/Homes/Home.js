10d7261b3988db6554b271bcd6768beeb2a04b59
rhnchowdhury <rhnchdri@gmail.com>
2023-11-15 15:01:46 +0600

    news section add

--- src/Components/Layouts/Homes/Home.js
+++ src/Components/Layouts/Homes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Carousel from '../../Pages/Carousel/Carousel';
 import Story from '../../Pages/Story/Story';
+import News from '../../Pages/News/News';
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
             <Story></Story>
+            <News></News>
         </div>
     );
 };
