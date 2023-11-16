9e6d45e090a6e682f589be34db50dc88b0e19cab
rhnchowdhury <rhnchdri@gmail.com>
2023-11-16 16:14:42 +0600

    mission middle part edit

--- src/Components/Pages/Missions/Mission.js
+++ src/Components/Pages/Missions/Mission.js
@@ -6,13 +6,14 @@ import img3 from '../../../assets/home-imgs/mission-img3.png';
 import imgBg from '../../../assets/logos/frame-2.svg';
 import imgBg2 from '../../../assets/logos/frame-4.svg';
 import imgBg3 from '../../../assets/home-imgs/school-bg.png';
+import imgBg4 from '../../../assets/logos/frame-5.svg';
 
 const Mission = () => {
     return (
         <div className='mt-12'>
             <h2 className='mission-h2 '>Mission & Vision</h2>
             <div className='relative'>
-                <img src={imgBg} alt="" className='opacity-50' />
+                <img src={imgBg} alt="" className='opacity-50 -z-[1000]' />
             </div>
             <div className='-mt-[900px]'>
                 <div className='ml-12 grid grid-cols-2 gap-36'>
@@ -42,8 +43,8 @@ const Mission = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex'>
-                <div className=' z-50'>
+            <div className='flex -z-50'>
+                <div className=' '>
                     <img src={imgBg3} alt="" className='mt-60 relative w-[1200px] h-[600px] mission-bg-img ml-32 ' />
                     {/* <img src={imgBg3} alt="" className='mt-60 relative h-[400px] mission-bg-img ml-32 ' /> */}
                     <div className='-mt-[794px] absolute'>
@@ -52,21 +53,25 @@ const Mission = () => {
                     </div>
                 </div>
                 <div>
-                    <div className='relative z-0'>
-                        <img src={imgBg2} alt="" className='w-[1800px] -z-10' />
+                    <div className='relative -z-40'>
+                        <img src={imgBg2} alt="" className='w-[1800px]' />
                         <div className=' -mt-[450px] absolute ml-72 mr-6'>
                             {/* <div className='-mt-[450px] absolute ml-72 mr-6'> */}
-                            <h2 className='mission-sec-h2 text-[#24316B] -mt-96'>Khukumoni <br /> School</h2>
-                            <p className='mission-p mt-8'>Free Primary School Education for underprivileged children, where we teach more than 5 hundred children in our 5 school branches around the Dhaka city. Location of schools are given below:
+                            <h2 className='mission-sec-h2 -mt-96'>Khukumoni <br /> School</h2>
+                            <p className='mission-p mt-8 text-[#24316B]'>Free Primary School Education for underprivileged children, where we teach more than 5 hundred children in our 5 school branches around the Dhaka city. Location of schools are given below:
                                 <br /> a) Karwan Bazar Railway Slum(beside Tejgaon Truck Stand) <br /> b) Nandipara Lane no 1, Bashabo. <br /> c) Italy Market, Sonir Akhra. <br /> d) Road 12/A, Dhanmondi Lakeshore. <br /> e) Launch Terminal, Sadarghat.
                             </p>
                             <button className="mission-btn mt-8">Learn More</button>
                         </div>
                     </div>
-
-
                 </div>
             </div>
+            <div className='-mt-[500px] z-50 absolute'>
+                <div className=''>
+                    <img src={imgBg4} alt="" className='' />
+                </div>
+            </div>
+
         </div >
     );
 };
