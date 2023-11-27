b3b0201ce4d48dc49f092640358d9ae31665e0e6
rhnchowdhury <rhnchdri@gmail.com>
2023-11-27 15:22:08 +0600

    programs all added

--- src/Components/Pages/Programs/Program.js
+++ src/Components/Pages/Programs/Program.js
@@ -1,26 +1,172 @@
 import React from 'react';
-import img1 from '../../../assets/home-imgs/project-img1.png';
-import img2 from '../../../assets/home-imgs/project-img2.png';
+import img1 from '../../../assets/program/prog-img1.png';
+import img2 from '../../../assets/program/prog-img2.png';
+import img3 from '../../../assets/program/prog-img3.png';
+import img4 from '../../../assets/program/prog-img4.png';
+import img5 from '../../../assets/program/prog-img5.png';
+import img6 from '../../../assets/program/prog-img6.png';
+import img7 from '../../../assets/program/prog-img7.png';
+import img8 from '../../../assets/program/prog-img8.png';
+import img9 from '../../../assets/program/prog-img9.png';
+import img10 from '../../../assets/program/prog-img10.png';
+import img11 from '../../../assets/program/prog-img11.png';
+import './Program.css';
 
 const Program = () => {
     return (
-        <div className='m-6 grid gap-2'>
-            <div className="card w-96">
-                <figure><img src={img1} alt="Shoes" className='relative' /></figure>
-                <div className="card-body absolute top-48 text-white ">
-                    <h2 className="card-title text-5xl text-center font-bold">Khukumoni Stipends</h2>
+        <div className='my-10 grid gap-5'>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row ">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Food For All</h1>
+                        <img src={img1} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='ml-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">Khukumoni foundation designed the food for all programs only for poor and unprivileged people throughout Bangladesh to remove their distress and food crisis. Since 2016 we have been providing food for the poor and helpless people, which is distributed in exchange for only One Taka.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
                 </div>
             </div>
-            <div className="card w-96">
-                <figure><img src={img2} alt="Shoes" className='relative' /></figure>
-                <div className="card-body absolute top-60 text-white ">
-                    <h2 className="card-title text-5xl text-center font-bold">Food For All</h2>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Corona Pandemic</h1>
+                        <img src={img2} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='mr-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">COVID-19 exhibits not only a health crisis in Bangladesh but also make severe food crisis among the impoverished especially in Dhaka city. The humanitarian crisis is on the going in whole, Bangladesh is also suffering from humanitarian crises due to this pandemic.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
                 </div>
             </div>
-
-            <div className="card w-96 bg-[#24316B] text-center">
-                <div className="card-body text-center">
-                    <h2 className="card-title text-7xl  font-bold text-white">More...</h2>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Free Education Programme</h1>
+                        <img src={img3} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='ml-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">Khukumoni foundation believes only quality education can uplift Bangladesh toward the progress in the social and economic sectors. But it requires ensuring quality education among the children throughout the city, slum, and remote arena.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Free Health Camping</h1>
+                        <img src={img4} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='mr-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">We are delighted to announce that the Khukumoni foundation take initiative to start free medical health camping to the betterment of the underprivileged community. Our health camps also make sure that people are taking healthcare at the right time and seeing the doctor earlier enough before falling in a critical situation.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Sponsor a Child</h1>
+                        <img src={img5} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='ml-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">Sponsoring a child in Bangladesh is a life-changing opportunity to make a positive impact. By offering your support, you provide a child with access to education, healthcare, nutrition, and a brighter future.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Skill Development</h1>
+                        <img src={img6} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='mr-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">Skill development is the process of identifying your skill gaps, and Development and sharpening these skills. It is important because your skills determine your ability to execute your plans with success.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div>
+                        <h1 className='text-3xl font-bold'>New Dress Giving Program</h1>
+                        <img src={img7} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='ml-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">At different festivals, we offer new clothes for needy children and the elderly for 5 takas. In this attempt, they are being filled with joy and ecstasy. We want everyone on earth to be equally joyful on different festivals or religious occasions.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Khukumoni Stipends</h1>
+                        <img src={img8} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='mr-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">Stipends in Bangladesh play a crucial role in promoting education and alleviating poverty. The government of Bangladesh, along with various non-governmental organizations, offers stipends to students at different levels of education, particularly in primary and secondary schools.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Cleansing Program</h1>
+                        <img src={img9} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='ml-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">COVID-19 exhibits not only a health crisis in Bangladesh but also make severe food crisis among the impoverished especially in Dhaka city. The humanitarian crisis is on the going in whole, Bangladesh is also suffering from humanitarian crises due to this pandemic.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Warm Clothes For Poor People</h1>
+                        <img src={img10} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='mr-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">COVID-19 exhibits not only a health crisis in Bangladesh but also make severe food crisis among the impoverished especially in Dhaka city. The humanitarian crisis is on the going in whole, Bangladesh is also suffering from humanitarian crises due to this pandemic.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 mx-20">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div>
+                        <h1 className='text-3xl font-bold'>Giving Relief To Flood victims</h1>
+                        <img src={img11} alt='' className="max-w-xs rounded-lg mt-6" />
+                    </div>
+                    <div className='ml-20'>
+                        <h1 className="text-2xl text-[#24316B] font-bold">Project Info</h1>
+                        <p className="py-6 program-p">COVID-19 exhibits not only a health crisis in Bangladesh but also make severe food crisis among the impoverished especially in Dhaka city. The humanitarian crisis is on the going in whole, Bangladesh is also suffering from humanitarian crises due to this pandemic.
+                        </p>
+                        <button className="btn program-btn">See Details</button>
+                    </div>
                 </div>
             </div>
         </div>
