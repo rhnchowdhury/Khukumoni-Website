ad3884096943a01a0ba9d8b620f4c6e9a4cf1aa4
rhnchowdhury <rhnchdri@gmail.com>
2023-11-29 16:22:11 +0600

    some edit

--- src/Components/Pages/HomePages/Carousel/Carousel.js
+++ src/Components/Pages/HomePages/Carousel/Carousel.js
@@ -45,14 +45,6 @@ const Carousel = () => {
                 <SwiperSlide>
                     <img src={img5} alt="" className='w-full h-[548px]' />
                 </SwiperSlide>
-                {/* <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide> */}
             </Swiper>
         </>
     );
