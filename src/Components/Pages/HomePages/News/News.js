f02a351dc0bec9765334cb4410180d69cc2fb94d
rhnchowdhury <rhnchdri@gmail.com>
2023-12-14 12:17:29 +0600

    edit on footer page

--- src/Components/Pages/HomePages/News/News.js
+++ src/Components/Pages/HomePages/News/News.js
@@ -12,7 +12,7 @@ import vdo4 from '../../../../assets/videos/71 tv.mp4';
 
 const News = () => {
     return (
-        <div className='mt-16 mx-16'>
+        <div className='mt-16 mx-12'>
             <h2 className='news-h2'>Khukumoni Foundation’s on News</h2>
             <>
                 <Swiper
@@ -42,7 +42,7 @@ const News = () => {
                     <SwiperSlide>
                         <div className='flex justify-center gap-8'>
                             <div>
-                                <video loop controls className='w-[550px] h-[285px] rounded-2xl'>
+                                <video loop controls className='w-[500px] h-[285px] rounded-2xl'>
                                     <source src={vdo3} type='video/mp4' />
                                 </video>
                                 <h1 className='mt-4 text-center text-2xl font-bold'>Desh TV</h1>
--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -15,19 +15,19 @@ const Footer = () => {
     return (
         <div className='mt-8'>
             <div className="relative  bg-[#18214E] w-full h-[430px] overflow-hidden text-left text-sm text-white ">
-                <div className="absolute top-[405px] left-[635px] w-[171px] h-4 text-md">
+                <div className="absolute top-[405px] left-[523px] w-[171px] h-4 text-md">
                     <div className="absolute top-[6.25%] left-[0%] font-light">
                         © 2023 All Rights Reserved
                     </div>
                 </div>
-                <div className="absolute top-[380px] left-[453px] flex flex-row items-start justify-start gap-[40px] font-medium">
+                <div className="absolute top-[380px] left-[350px] flex flex-row items-start justify-start gap-[40px] font-medium">
                     <div className="relative">Privacy Policy</div>
                     <div className="relative">Terms of Use</div>
                     <div className="relative">Sales and Refunds</div>
                     <div className="relative">Legal</div>
                     <div className="relative">Site Map</div>
                 </div>
-                <div className="absolute top-[230px] left-[622px] w-[251px] h-[38px]">
+                <div className="absolute top-[230px] left-[535px] w-[251px] h-[38px]">
                     <div className="absolute top-[10px] left-[37px] w-[214px] h-[38px]">
                         <div className="absolute top-[9.89%] left-[0%]">
                             <p className="m-0">R.H Home Center, Green Road, Farmgate , Dhaka, Bangladesh</p>
@@ -35,7 +35,7 @@ const Footer = () => {
                     </div>
                     <FaMapMarkerAlt className="absolute top-[16px] left-[0px] w-6 h-6 overflow-hidden"></FaMapMarkerAlt>
                 </div>
-                <div className="absolute top-[290px] left-[622px] w-[186px] h-6">
+                <div className="absolute top-[290px] left-[535px] w-[186px] h-6">
                     <div className="absolute top-[2px] left-[37px] w-[149px] h-5">
                         <div className="absolute top-[5%] left-[0.67%]">
                             Info@khukumoni.com
@@ -43,12 +43,12 @@ const Footer = () => {
                     </div>
                     <GoMail className="absolute top-[0px] left-[0px] w-6 h-6 overflow-hidden"></GoMail>
                 </div>
-                <b className="absolute top-[210px] left-[622px] text-[18px]">Contact Us</b>
-                <div className="absolute top-[330px] left-[622px] rounded-md bg-white shadow-[0px_4px_6px_-4px_rgba(24,_39,_75,_0.12),_0px_8px_8px_-4px_rgba(24,_39,_75,_0.08)] box-border w-[150px] h-[30px] flex flex-row items-center justify-center py-[5px] px-[26px] text-center text-base text-black border-[1px] border-solid border-white">
+                <b className="absolute top-[210px] left-[530px] text-[18px]">Contact Us</b>
+                <div className="absolute top-[330px] left-[535px] rounded-md bg-white shadow-[0px_4px_6px_-4px_rgba(24,_39,_75,_0.12),_0px_8px_8px_-4px_rgba(24,_39,_75,_0.08)] box-border w-[150px] h-[30px] flex flex-row items-center justify-center py-[5px] px-[26px] text-center text-base text-black border-[1px] border-solid border-white">
                     <div className="relative">Subscribe</div>
                 </div>
                 <img src={bgImg1} alt="" className="absolute h-[94.63%] w-[41.17%] top-[0%] right-[-3.05%] bottom-[15.37%] left-[61.88%] max-w-full overflow-hidden max-h-full opacity-[0.35]" />
-                <div className="absolute h-[68.71%] w-[25.97%] top-[18.14%] right-[66.18%] bottom-[18.14%] left-[3.85%] text-lg">
+                <div className="absolute h-[68.71%] w-[25.97%] top-[18.14%] right-[66.18%] bottom-[18.14%] left-[1.85%] text-lg">
                     <b className="absolute top-[0%] left-[31.82%]">Home</b>
                     <b className="absolute top-[13.35%] left-[31.82%]">About Us</b>
                     <b className="absolute top-[25.69%] left-[31.82%]">Our Program</b>
@@ -57,7 +57,7 @@ const Footer = () => {
                     <b className="absolute top-[60.39%] left-[31.82%]">Team</b>
                     <b className="absolute top-[71.68%] left-[31.82%]">Contact</b>
                 </div>
-                <img src={logo} alt="" className="absolute top-[50px] left-[623px] w-[150px] h-[160px] overflow-hidden" />
+                <img src={logo} alt="" className="absolute top-[50px] left-[523px] w-[150px] h-[160px] overflow-hidden" />
                 <div className="absolute w-[17.01%] right-[0%] bottom-[0px] left-[82.99%] bg-[#24316B] h-[60px] flex flex-col items-end justify-center py-[27px] px-[38px] box-border">
                     <div className="h-[18.11px] flex flex-row items-center justify-end gap-[17px]">
                         <BsInstagram className='nav-icon'></BsInstagram>
@@ -70,7 +70,7 @@ const Footer = () => {
                     <img src={img2} alt="" className="absolute h-[60%] w-[40%] top-[18%] right-[45.11%] bottom-[3.14%] left-[5.6%] max-w-full overflow-hidden max-h-full object-cover" />
                     <img src={img1} alt="" className="absolute h-[70%] w-[45%] top-[7.15%] right-[4.02%] bottom-[2.26%] left-[36.82%] max-w-full overflow-hidden max-h-full object-cover" />
                 </div>
-                <div className="absolute top-[270px] left-[1060px] w-[250px]  text-[20px]">
+                <div className="absolute top-[270px] left-[950px] w-[250px]  text-[20px]">
                     <div className="absolute   rounded-xl bg-[#24316B] box-border w-[253px] h-[60px] border-[4px] border-solid border-[#6286C4] " />
                     <Link><b className="absolute top-[18px] left-[65px]">Donate Now</b></Link>
                 </div>
