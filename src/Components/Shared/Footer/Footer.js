5b0447ceb85e9d353815011790b84195f56124d6
rhnchowdhury <rhnchdri@gmail.com>
2023-12-24 16:39:45 +0600

    some edit

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -46,7 +46,9 @@ const Footer = () => {
                 </div>
                 <b className="absolute top-[210px] left-[530px] text-[18px]">Contact Us</b>
                 <div className="absolute top-[330px] left-[535px] rounded-md bg-white shadow-[0px_4px_6px_-4px_rgba(24,_39,_75,_0.12),_0px_8px_8px_-4px_rgba(24,_39,_75,_0.08)] box-border w-[150px] h-[30px] flex flex-row items-center justify-center py-[5px] px-[26px] text-center text-base text-black border-[1px] border-solid border-white">
-                    <div className="relative">Subscribe</div>
+                    <div className="relative cursor-pointer">
+                        <a href="https://www.youtube.com/@KhukumoniFoundation-kc1uq" target="_blank" rel="noreferrer noopener">Subscribe</a>
+                    </div>
                 </div>
                 <img src={bgImg1} alt="" className="absolute h-[94.63%] w-[41.17%] top-[0%] right-[-3.05%] bottom-[15.37%] left-[61.88%] max-w-full overflow-hidden max-h-full opacity-[0.35]" />
                 <div className="absolute h-[68.71%] w-[25.97%] top-[18.14%] right-[66.18%] bottom-[18.14%] left-[1.85%] text-lg">
@@ -62,7 +64,6 @@ const Footer = () => {
                 <div className="absolute w-[17.01%] right-[0%] bottom-[0px] left-[82.99%] bg-[#24316B] h-[60px] flex flex-col items-end justify-center py-[27px] px-[38px] box-border">
                     <div className="h-[18.11px] flex flex-row items-center justify-end gap-[17px]">
                         <BsInstagram className='nav-icon'></BsInstagram>
-
                         <a href="https://www.facebook.com/khukumonifoundation" target="_blank" rel="noreferrer noopener"><BsFacebook className='nav-icon'></BsFacebook></a>
                         <a href="https://www.youtube.com/@KhukumoniFoundation-kc1uq" target="_blank" rel="noreferrer noopener"><IoLogoYoutube className='nav-icon' /></a>
                         <a href="https://www.linkedin.com/in/khukumoni-foundation-21041a192/" target="_blank" rel="noreferrer noopener"><FaLinkedin className='nav-icon'></FaLinkedin></a>
