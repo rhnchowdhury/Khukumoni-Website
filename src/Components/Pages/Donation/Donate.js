02a8b78c9d18e7cc7662e639274006408bd03f34
rhnchowdhury <rhnchdri@gmail.com>
2023-12-26 17:21:35 +0600

    donation section done

--- src/Components/Pages/Donation/Donate.js
+++ src/Components/Pages/Donation/Donate.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import img1 from '../../../assets/logos/ssl-img.jpg';
 
 const Donate = () => {
     return (
         <section className='m-5 lg:m-10 p-5 lg:py-2 '>
             <div className='flex justify-center items-center my-10 '>
                 <div className='bg-[#253E88] p-10 rounded-xl'>
-                    {/* bg-[#253E88] */}
                     <form >
-
                         <div>
                             <h1 className='text-white text-3xl font-bold'>Donation Information</h1>
                             <p className='text-white mt-4'>How much would you like to donate?</p>
@@ -39,12 +38,8 @@ const Donate = () => {
                         <div className='mt-2 flex'>
                             <input type="radio" name="radio-1" className="" />
                             <p className='text-white ml-1'>Custom amount (BDT)</p>
-
                         </div>
                         <input type='text' className="input input-bordered w-full max-w-xs" />
-
-
-
                     </form>
                     <div>
                         <div className='mt-4'>
@@ -60,6 +55,13 @@ const Donate = () => {
                                 <option>Eid Gift</option>
                                 <option>প্রজেক্ট নিজ পায়ে দাঁড়াই</option>
                             </select>
+                        </div>
+                        <div className="card-actions justify-end mt-4">
+                            <button className="btn bg-[#4268B1] hover:bg-[#24316B] border-none text-white">Donate Now</button>
+                        </div>
+                        <div>
+                            <h1 className='text-white'>We Accept</h1>
+                            <img src={img1} alt="" className='w-80 mt-4 rounded-md' />
                         </div>
                     </div>
                 </div>
